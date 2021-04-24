f96cb86ac1671ed617ec67ff3b7e9f59ccc7ab0a
Luke 10x <spam@luke10x.dev>
2021-04-24 11:45:11 +0100

    🔥 These should not be commited

--- config/lua/keymap.lua
+++ config/lua/keymap.lua
@@ -14,6 +14,22 @@ map('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>', options)
 -- map('n', '<C-p>', ':Telescope k<cr>', options)
 
 
--- nvim-tree
-map('n', '<leader>tt', ':NvimTreeToggle<CR>', options)
-map('n', '<leader>ff', ':NvimTreeFindFile<CR>', options)
+-- buffers
+map('n', '[b', ':BufferLineCyclePrev<CR>', options)
+map('n', ']b', ':BufferLineCycleNext<CR>', options)
+
+
+-- Sends text selected in visualmode to termbin
+local result = vim.api.nvim_exec([[
+xnoremap <leader>tb <esc>:'<,'>:w ! nc termbin.com 9999<CR>
+]], true)
+
+-- Git Gutter
+map('n', '<leader>g', ':GitGutterToggle<CR>', options)
+map('n', '<leader>ha', ':GitGutterToggle<CR>', options)
+-- highlight clear SignColumn
+-- "nmap <Leader>ha <Plug>GitGutterStageHunk
+-- "nmap <Leader>hu <Plug>GitGutterRevertHunk
+
+-- " Fugitive
+-- nmap <leader>gb :Gblame<CR>
